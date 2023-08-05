af0bb0a5eadfb7d3c635e05913993e3debf20b10
xiaoyi7891 <141465950+xiaoyi7891@users.noreply.github.com>
2023-08-05 09:31:27 +0800

    Update wangshu.js

--- wangshu.js
+++ wangshu.js
@@ -8,7 +8,7 @@
 
 [rewrite_local]
 
-^https:\/\/api-v3\.speedtest\.cn\/user\/info url script-response-body https://raw.githubusercontent.com/chxm1023/Rewrite/main/wsgj.js
+^https:\/\/api-v3\.speedtest\.cn\/user\/info url script-response-body https://raw.githubusercontent.com/xiaoyi7891/wangshu/main/wangshu.js
 
 [mitm]
 
